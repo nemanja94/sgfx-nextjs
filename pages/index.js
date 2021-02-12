4382fd5c0124e070d34e05d9221eaddfbc9cfc31
nemanja94 <aandric.nemanja@gmail.com>
2021-02-12 22:04:23 +0100

    ...

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Carousel></Carousel>
-     
+      <div className={styles.title}>
+        <h1>Sve sto vam treba da svoj posao predstavite na profesionalan nacin</h1>
+        <p>Najjace oruzje u marketingu, promovisite svoj posao kroz profesionalne show-real video prezentacije. Reklame, postovi za drustvene medije i baneri ce vam takodje trebati, a mi vam mozemo pomoci u tome</p>
+      </div>
+      <div className={styles.cards}>
+        <div className={styles.card}>
+          <div className={styles.card_title}>
+            <h1>Korporativni dizajn</h1>
+          </div>
+          <div className={styles.card_content}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend lacus et odio maximus ultrices. In hac habitasse platea dictumst. Vivamus ullamcorper rhoncus dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend lacus et odio maximus ultrices. In hac habitasse platea dictumst. Vivamus ullamcorper rhoncus dictum.</p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.card_title}>
+            <h1>Korporativni dizajn</h1>
+          </div>
+          <div className={styles.card_content}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend lacus et odio maximus ultrices. In hac habitasse platea dictumst. Vivamus ullamcorper rhoncus dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend lacus et odio maximus ultrices. In hac habitasse platea dictumst. Vivamus ullamcorper rhoncus dictum.</p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.card_title}>
+            <h1>Korporativni dizajn</h1>
+          </div>
+          <div className={styles.card_content}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend lacus et odio maximus ultrices. In hac habitasse platea dictumst. Vivamus ullamcorper rhoncus dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend lacus et odio maximus ultrices. In hac habitasse platea dictumst. Vivamus ullamcorper rhoncus dictum.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
